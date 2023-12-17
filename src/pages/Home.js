69c36f902535dddb26e28527139ad3f53ce34301
Minh Tran <minhtran344@gmail.com>
2023-12-17 18:02:13 +0000

    Add homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,19 @@ function Home() {
   return (
     <div>
     <App></App>
-    <p>Home</p>
+    <div className="homeContainer">
+      <h1>Welcome To The TOMODO Productive Web App</h1>
+      <p>Please navigate to either <b>In-progress Tasks</b> or <b>Paramodo</b> in the nav bar to use our service.
+      </p>
+
+            <img
+              alt=""
+              src={require("../assets/imgs/Tomodo.gif")}
+              width={400}
+            />{' '}
+
+    </div>
+    
     </div>
   );
 }
